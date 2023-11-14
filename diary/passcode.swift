c22dbb4f9b4661d391507f8175db2977178804a9
dodixwannacry <132576872+dodixwannacry@users.noreply.github.com>
2023-11-14 14:03:49 +0100

    ss

---
+++ diary/passcode.swift
@@ -0,0 +1,18 @@
+//
+//  passcode.swift
+//  diary
+//
+//  Created by Rodolfo Falanga on 14/11/23.
+//
+
+import SwiftUI
+
+struct passcode: View {
+    var code: String = "" {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    passcode()
+}
