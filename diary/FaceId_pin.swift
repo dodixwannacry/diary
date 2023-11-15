1c442bdbfa41a2d5d3939ec8a34aeca90df733cc
dodixwannacry <132576872+dodixwannacry@users.noreply.github.com>
2023-11-15 11:00:40 +0100

    face id

---
+++ diary/FaceId_pin.swift
@@ -0,0 +1,46 @@
+//
+//  FaceId_pin.swift
+//  diary
+//
+//  Created by Rodolfo Falanga on 15/11/23.
+//
+
+import SwiftUI
+import LocalAuthentication
+
+struct FaceId_pin: View {
+    @State private var unlocked = false
+    @State private var text = "LOCKED"
+    var body: some View {
+        VStack {
+            Text(text)
+                .bold()
+                .padding()
+            
+            Button("Authenticate"){
+                authenticate()
+                
+            }
+        }
+    }
+    func authenticate(){
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics,error: &error){
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "This is for security reason"   ){success, authenticationError in
+                
+                if success {
+                    text = "Unlocked"
+                } else{
+                    text = "There was a problem!"
+                }
+            }
+        } else {
+            text = "Phone has not biometrics"
+        }
+    }
+}
+    #Preview {
+        FaceId_pin()
+    }
